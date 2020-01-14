19d355fd40cf27124ccf33ee4ee5b4ec22c0220c
Bloch Tom <35507756+TheThingGoesSkra@users.noreply.github.com>
2020-01-14 17:38:50 +0100

    Delete proxy.fish

--- functions/proxy.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function proxy --description 'alias proxy export http_proxy=http://127.0.0.1:8080;export https_proxy=http://127.0.0.1:8080'
-	export http_proxy=http://127.0.0.1:8080;export https_proxy=http://127.0.0.1:8080 $argv;
-end
